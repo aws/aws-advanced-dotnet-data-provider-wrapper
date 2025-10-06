d5fc488acd5df912fefd542d4b5cc23d8d6b2c8a
kenrick.yap@improving.com <14yapkc1@gmail.com>
2025-10-05 21:38:34 -0700

    removed unused imports

--- AwsWrapperDataProvider.Tests/Driver/Dialects/DialectProviderTests.cs
+++ AwsWrapperDataProvider.Tests/Driver/Dialects/DialectProviderTests.cs
@@ -1,9 +1,20 @@
-using System;
-using System.Collections.Generic;
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System.Data;
 using System.Data.Common;
 using AwsWrapperDataProvider.Driver;
-using AwsWrapperDataProvider.Driver.Configuration;
 using AwsWrapperDataProvider.Driver.Dialects;
 using AwsWrapperDataProvider.Driver.Exceptions;
 using AwsWrapperDataProvider.Driver.HostInfo;
@@ -11,7 +22,6 @@ using AwsWrapperDataProvider.Driver.Utils;
 using Moq;
 using MySqlConnector;
 using Npgsql;
-using Xunit;
 
 namespace AwsWrapperDataProvider.Tests.Driver.Dialects;
 
