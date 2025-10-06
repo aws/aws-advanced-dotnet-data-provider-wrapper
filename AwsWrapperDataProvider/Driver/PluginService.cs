a8d88186f5c1bfa732eeb09f1a8d49a30e1d9c6e
kenrick.yap@improving.com <14yapkc1@gmail.com>
2025-10-06 09:51:23 -0700

    force open

--- AwsWrapperDataProvider/Driver/PluginService.cs
+++ AwsWrapperDataProvider/Driver/PluginService.cs
@@ -271,9 +271,9 @@ public class PluginService : IPluginService, IHostListProviderService
         return this.pluginManager.Open(hostSpec, props, this.CurrentConnection == null, pluginToSkip);
     }
 
-    public DbConnection OpenConnection(HostSpec hostSpec, Dictionary<string, string> props, IConnectionPlugin? pluginToSkip, bool isInitialConnection)
+    public DbConnection ForceOpenConnection(HostSpec hostSpec, Dictionary<string, string> props, IConnectionPlugin? pluginToSkip, bool isInitialConnection)
     {
-        return this.pluginManager.Open(hostSpec, props, isInitialConnection, pluginToSkip);
+        return this.pluginManager.ForceOpen(hostSpec, props, isInitialConnection, pluginToSkip);
     }
 
     public DbConnection ForceOpenConnection(HostSpec hostSpec, Dictionary<string, string> props, IConnectionPlugin? pluginToSkip)
