0e034adfd2375fbef6aaf2edcbeafe9d7b343226
Yan Wang <yan.wang@improving.com>
2026-03-24 11:16:22 -0700

    bump versions

--- AwsWrapperDataProvider.NHibernate.Tests/AwsWrapperDriverTests.cs
+++ AwsWrapperDataProvider.NHibernate.Tests/AwsWrapperDriverTests.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Data;
 using System.Reflection;
 using AwsWrapperDataProvider.Driver.Plugins.Failover;
 using AwsWrapperDataProvider.Tests;
